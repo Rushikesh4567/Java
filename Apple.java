d14c0c1b3e9f203cd2e1be0d20119864945232b0
Rushikesh Shivanand Anantpure <rushikeksh4567@gmail.com>
2025-07-11 06:53:35 +0530

    Add files via upload

---
+++ Apple.java
@@ -0,0 +1,13 @@
+package com.app.fruits;
+
+public class Apple extends Fruits {
+
+    public Apple(String name, String color, double weight) {
+        super(name, color, weight);
+    }
+
+    @Override
+    public String taste() {
+        return "sweet n sour.";
+    }
+}
---
+++ Mango.java
@@ -0,0 +1,13 @@
+package com.app.fruits;
+
+public class Mango extends Fruits {
+
+    public Mango(String name, String color, double weight) {
+        super(name, color, weight);
+    }
+
+    @Override
+    public String taste() {
+        return "sweet.";
+    }
+}
