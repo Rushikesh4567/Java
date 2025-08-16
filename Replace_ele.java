77488caad2f4532e8574c2e00cdc4ce39796e285
Rushikesh Shivanand Anantpure <rushikeksh4567@gmail.com>
2025-08-16 21:40:32 +0530

    Add files via upload

---
+++ Replace_ele.java
@@ -0,0 +1,29 @@
+package com.collect;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Replace_ele
+{
+
+	public static void main(String[] args) 
+	{
+		List<String> list = new ArrayList<String>();
+		
+		list.add("Apple");
+		list.add("Mango");
+		list.add("Banana");
+		list.add("Orange");
+		
+		
+
+		System.out.println("Original List is "+list);
+	
+		String ele = "Replaced";
+		list.set(3, ele);
+		
+		System.out.println("After Replacing the element in List is "+list);
+		
+	}
+
+}
